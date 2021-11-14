6cbba1352c2250807cfe70f5ba19589b4852ce7c
Roman Rodionov <kpackl14@gmail.com>
2021-11-14 20:53:20 +0700

    Add MaeinMenu

---
+++ Game Farm/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    public GameObject tile;
+    public Vector2 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
