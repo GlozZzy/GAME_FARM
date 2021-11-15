52e42d033a9fac6c5649263c0a4ecebb0a3865b1
Roman Rodionov <kpackl14@gmail.com>
2021-11-15 16:45:54 +0700

    Add empty Classes

--- Game Farm/Assets/Scripts/AnimalEnclosure.cs
+++ Game Farm/Assets/Scripts/AnimalEnclosure.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Field : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
