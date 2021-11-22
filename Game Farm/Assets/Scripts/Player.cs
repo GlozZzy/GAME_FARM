db82dcc338faa5d7d250ec6c67e398ebfa7115a7
Deakib <72688319+Deakib@users.noreply.github.com>
2021-11-22 15:40:33 +0700

    PlantGrow
    Добавил посадку, рост и сбор растений

--- Game Farm/Assets/Scripts/Player.cs
+++ Game Farm/Assets/Scripts/Player.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public int money = 100;
+    public Text moneyTxt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moneyTxt.text = "$" + money;
     }
 
     // Update is called once per frame
-    void Update()
+    public void Transaction(int value)
     {
-        
+        money += value;
+        moneyTxt.text = "$" + money;
     }
+    
 }
