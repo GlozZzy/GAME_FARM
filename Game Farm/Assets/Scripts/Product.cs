49b1b3a82a80ce3642e733d706da7b52fa146a82
GlozZzy <kpackl14@gmail.com>
2021-12-11 21:47:26 +0700

    add interface and warehouse logic

--- Game Farm/Assets/Scripts/Product.cs
+++ Game Farm/Assets/Scripts/Product.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Product : MonoBehaviour
 {
+    public string Pname;
+    public int price;
+    public string description;
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Game Farm/Assets/Scripts/WareHouse.cs
+++ Game Farm/Assets/Scripts/WareHouse.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WareHouse : MonoBehaviour
 {
+    public Player player;
     public Canvas CanvasMenu;
+    public Text spaceText;
+    public int maxspace;
+
+    private int curspace;
+    private List<Product> products;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        curspace = 0;
+        products = new List<Product>();
+        spaceText.text = "Space: " + curspace + "/" + maxspace;
     }
 
     // Update is called once per frame
@@ -32,5 +42,35 @@ public class WareHouse : MonoBehaviour
     public void CloseMenu()
     {
         CanvasMenu.enabled = false;
+    }
+
+    public void Expand()
+    {
+        if (player.Transaction(-100))
+        {
+            maxspace += 10;
+            spaceText.text = "Space: " + curspace + "/" + maxspace;
+        }
+        else print("NotEnougt money");
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+        curspace++;
+    }
+
+    public void SellProduct(Text product)
+    {
+        int ind = -1;
+        for (int i = 0; i < products.Count; i++)
+            if (products[i].name == product.text)
+                ind = i;
+        if (ind >= 0) 
+        {
+            player.Transaction(products[ind].price);
+            curspace--;
+            products.RemoveAt(ind);
+        }
     }
 }
